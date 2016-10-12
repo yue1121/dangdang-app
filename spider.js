4e67111655a1500c878581d331e0810bf7659ad3
yuluo <yuluo@yuluodeMacBook-Pro.local>
2016-10-12 17:40:04 +0800

    把抓取的数据写入文件

--- spider.js
+++ spider.js
@@ -1,6 +1,6 @@
 var Crawler = require("crawler")
 var url = require('url')
-
+var fs = require('fs')
 
 // 通过设置forceUTF8和incomingEncoding处理中文编码页面的问题
 var c = new Crawler({
@@ -9,13 +9,14 @@ var c = new Crawler({
     incomingEncoding:'gb2312'
 })
 
-
+var arrBookErTong = []
 getData('http://bang.dangdang.com/books/newhotsales/01.41.00.00.00.00-24hours-0-0-1-',1)
 function getData(url,page){
 c.queue([{
     uri:url+page,
     callback:(err,result,$)=>{
-        console.log($('title').text())
+        console.log(result.uri)
+        //console.log($('title').text())
         $('.bang_list li').each((index,li)=>{
             var obj = {}
             obj.title = $(li).find('.name').text()
@@ -25,10 +26,12 @@ c.queue([{
             obj.publist_time = $(li).find('.publisher_info').eq(1).find('span').text()
             obj.link = $(li).find('.pic a').attr('href')
             obj.price = $(li).find('.price .price_n').text()
-            console.log(obj)
-            console.log('-------')
+           // console.log(obj)
+           // console.log('-------')
+            arrBookErTong.push(obj)
         })
         if(page>5){
+            fs.writeFileSync('./data/book_ertong.json',JSON.stringify(arrBookErTong))
             console.log('获取数据成功')
         }
         else{
