87dd2da304bcd44c2b091c9ffe2b21782c9d771d
yuluo <yuluo@yuluodeMacBook-Pro.local>
2016-10-12 21:09:30 +0800

    完善视图展示部分代码

--- app.js
+++ app.js
@@ -11,21 +11,45 @@ app.engine('.html', template.__express);
 app.set('view engine', 'html');
 app.set('views', './views');
 
-app.get('/types',(req,res)=>{
-    res.render('types',{types:[
-      {id:"ertong",name:"儿童"},
-      {id:"xiaoshuo",name:"小说"},
-      {id:"lishi",name:"历史"}
-  ]});
+// 书籍分类数据
+const bookTypes = [{
+    id: "ertong",
+    name: "儿童"
+}, {
+    id: "xiaoshuo",
+    name: "小说"
+}, {
+    id: "lishi",
+    name: "历史"
+}];
+
+app.get('/types', (req, res) => {
+    res.render('types', {
+        types: bookTypes
+    });
 })
 
-app.get('/list/:id',(req,res)=>{
-  var data = JSON.parse(fs.readFileSync(`./data/book_${req.params.id}.json`).toString());
-  res.render('list',{data:data});
+app.get('/list/:id', (req, res) => {
+    var data = [];
+    try{
+      data = JSON.parse(fs.readFileSync(`./data/book_${req.params.id}.json`).toString());
+    }
+    catch(err){
+      console.log(err);
+    }
+
+    // 筛选当前的数据分类信息
+    var bookType = bookTypes.find((item) => {
+        return item.id == req.params.id
+    })
+    res.render('list', {
+        data: data,
+        title: bookType.name
+    });
 })
 
 
 
-app.listen(3000,()=>{
-  console.log('服务器运行于3000端口');
+app.listen(3000, () => {
+    console.log('服务器运行于3000端口');
 })
